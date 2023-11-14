545f3d3cd95d44097592d03e1dd10b172ced580e
joystonley <andriajoy62@gmail.com>
2023-11-14 13:25:44 +0100

    initial commit

---
+++ apps/home/admin.py
@@ -0,0 +1,98 @@
+from django.contrib import admin
+
+from django.contrib.auth.models import Permission
+
+
+
+from .models import (
+    Categorie,
+    Diplome,
+    Contrat,
+    Conge,
+    TypeConge,
+    Grade,
+    Employe,
+    Situation,
+    Domaine,
+    Departement,
+    Avancement,
+    Fonction,
+    Mention,
+    Corps,
+    Enseignant,
+    Affectation,
+)
+
+# Register your models here.
+
+admin.site.register(Permission)
+
+@admin.register(Categorie)
+class CategorieAdmin(admin.ModelAdmin):
+    list_display = ('num_cat',)
+
+@admin.register(Diplome)
+class DiplomeAdmin(admin.ModelAdmin):
+    list_display = ('type_dip', 'description')
+
+@admin.register(Contrat)
+class ContratAdmin(admin.ModelAdmin):
+    list_display = ('type_con', 'dure_con')
+
+@admin.register(Conge)
+class CongeAdmin(admin.ModelAdmin):
+    list_display = ('date_debut', 'date_fin', 'type_conge', 'employe', 'enseignant')
+
+@admin.register(TypeConge)
+class TypeCongeAdmin(admin.ModelAdmin):
+    list_display = ('nom_tc', 'nb_jour')
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ('num_classe', 'num_echellon')
+
+@admin.register(Employe)
+class EmployeAdmin(admin.ModelAdmin):
+    list_display = ('nom_em', 'prenom_em', 'age', 'sex', 'im', 'date_nais', 'date_PF', 'cin', 'adresse', 'num_tel', 'indice', 'categorie', 'diplome', 'contrat', 'situation', 'domaine', 'departement', 'grade', 'fonction')
+
+@admin.register(Situation)
+class SituationAdmin(admin.ModelAdmin):
+    list_display = ('type_st',)
+
+@admin.register(Domaine)
+class DomaineAdmin(admin.ModelAdmin):
+    list_display = ('nom_dom', 'description')
+
+@admin.register(Departement)
+class DepartementAdmin(admin.ModelAdmin):
+    list_display = ('nom_dep', 'description')
+
+class AvancementAdmin(admin.ModelAdmin):
+    list_display = ('date_effective', 'employee', 'teacher')  # Assurez-vous d'avoir des attributs valides ici
+
+admin.site.register(Avancement, AvancementAdmin)
+
+
+@admin.register(Fonction)
+class FonctionAdmin(admin.ModelAdmin):
+    list_display = ('nom_fc',)
+
+@admin.register(Mention)
+class MentionAdmin(admin.ModelAdmin):
+    list_display = ('nom_me', 'description')
+
+@admin.register(Corps)
+class CorpsAdmin(admin.ModelAdmin):
+    list_display = ('nom_cr', 'description')
+
+@admin.register(Enseignant)
+class EnseignantAdmin(admin.ModelAdmin):
+    list_display = ('nom_en', 'prenom_en', 'age', 'sex', 'im', 'date_nais', 'date_PF', 'grade', 'diplome', 'corps', 'situation')
+
+
+class AffectationAdmin(admin.ModelAdmin):
+    list_display = ('date_aff', 'motif', 'employe')
+
+admin.site.register(Affectation, AffectationAdmin)
+
+    
---
+++ core/settings.py
@@ -0,0 +1,163 @@
+
+import os
+from decouple import config
+from unipath import Path
+# settings.py
+from django.db.models import BigAutoField
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = Path(__file__).parent
+CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', default=True, cast=bool)
+
+# load production server from .env
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'apps.home',
+    'import_export',
+    'chartjs',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+]
+
+ROOT_URLCONF = 'core.urls'
+LOGIN_REDIRECT_URL = "home"  # Route defined in home/urls.py
+LOGOUT_REDIRECT_URL = "home"  # Route defined in home/urls.py
+TEMPLATE_DIR = os.path.join(CORE_DIR, "apps/templates")  # ROOT dir for templates
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [TEMPLATE_DIR],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'core.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+# settings.py
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gr',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # Assurez-vous que le nom d'hôte est correct
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
+
+
+ # Remplacez 'myapp' par le nom de votre application et 'CustomUser' par le nom de votre modèle d'utilisateur personnalisé.
+
+# Password validation
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+from django.utils.translation import gettext_lazy as _
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(CORE_DIR, 'apps/static'),
+)
+
+# settings.py
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
+
+
+
+
+
+#############################################################
+#############################################################
